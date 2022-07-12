7b39efa20f025f82a6b993d0a4844b8583b0826f
OhHobin <hobin0676@daum.net>
2022-07-12 09:08:59 +0900

    up

--- VerilogHDL/modelsim/lab16_demux/src/rtl/demux.v
+++ VerilogHDL/modelsim/lab16_demux/src/rtl/demux.v
@@ -9,19 +9,35 @@ module demux(in, sel, out1, out2, out3, out4, out5, out6, out7, out8);
      begin
         if (sel == 3'h0)
             out1 = in;
-        else if (sel == 3'h1) 
+        else
+            out1 = 1'b0;
+        if (sel == 3'h1) 
             out2 = in;
-        else if (sel == 3'h2)
+        else
+            out2 = 1'b0;
+        if (sel == 3'h2)
             out3 = in;
-        else if (sel == 3'h3)
+        else
+            out3 = 1'b0;
+        if (sel == 3'h3)
             out4 = in;
-        else if (sel == 3'h4)
+        else
+            out4 = 1'b0;
+        if (sel == 3'h4)
             out5 = in;
-        else if (sel == 3'h5)
+        else
+            out5 = 1'b0;
+        if (sel == 3'h5)
             out6 = in;
-        else if (sel == 3'h6)
+        else
+            out6 = 1'b0;
+        if (sel == 3'h6)
             out7 = in;
         else
+            out7 = 1'b0;
+        if (sel == 3'h7)
             out8 = in;
+        else
+            out8 = 1'b0;
      end
 endmodule
--- VerilogHDL/modelsim/lab16_demux/testbench/testbench.v
+++ VerilogHDL/modelsim/lab16_demux/testbench/testbench.v
@@ -7,7 +7,7 @@ module testbench();
 
     demux dm(in, sel, out1, out2, out3, out4, out5, out6, out7, out8);
     initial begin
-        in = 1'b0;
+        in = 1'b1;
        
         sel = 3'h0;
         #10; sel = 3'h1;
